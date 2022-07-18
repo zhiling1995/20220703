b53ec5ff9e4b921feadbbf0d707b5da786642096
zhiling1995 <3353004045@qq.com>
2022-07-18 23:56:48 +0800

    在public  映入公共css 映入非路由组件Header Footer组件

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,26 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Header></Header>
     我是根组件11
+    <Footer></Footer>
+    
+
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default {
   name: 'App',
-  // components: {
-  //   HelloWorld
-  // }
+  components:{
+    Header,
+    Footer
+  }
+  
 }
 </script>
 
